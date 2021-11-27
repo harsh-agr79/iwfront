be7e5dcd146e03ee4588726cd0d2f779a85e5e6f
harsh-agr79 <agrharsh7932@gmail.com>
2021-11-27 19:08:04 +0545

    changes

--- assets/script.js
+++ assets/script.js
@@ -20,3 +20,18 @@ $(document).ready(function(){
       constrainWidth: false,
       coverTrigger:false,
   });
+
+//   var slider = document.getElementById('slider');
+//   noUiSlider.create(slider, {
+//    start: [20, 80],
+//    connect: true,
+//    step: 1,
+//    orientation: 'horizontal', // 'horizontal' or 'vertical'
+//    range: {
+//      'min': 0,
+//      'max': 100
+//    },
+//    format: wNumb({
+//      decimals: 0
+//    })
+//   });
