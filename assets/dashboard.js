5e3b588a5fd4277e2a7a49eb8a3f4981ca99f54d
Mobile House <sahilshrestha77@gmail.com>
2021-12-01 12:14:17 +0545

    date-time-fields

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -2,16 +2,38 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
 
     $('.modal').modal();
-
     var today = new Date()
     var year = today.getFullYear()
     var month = today.getMonth() + 1
     var day = today.getDate()
     var hour = today.getHours()
     var minutes = today.getMinutes()
-    var seconds = today.getSeconds()
-
-    var dateFormat = `${year}-${month}-${day}T${hour}:${minutes}:${seconds}`
+    var finalMonth = month + 3
+    var finalYear = year
+    
+    if(finalMonth>12){
+        finalMonth-=12
+        finalYear = year + 1
+    }
+    if(day<10){
+        day = '0'+day
+    }
+    if(month<10){
+        month = '0'+month
+    }
+    if(finalMonth<10){
+        finalMonth = '0'+finalMonth
+    }
+    if(hour<10){
+        hour = '0'+hour
+    }
+    if(minutes<10){
+        minutes = '0'+minutes
+    }
+    
+    var dateFormat = `${year}-${month}-${day}T${hour}:${minutes}`
+    var maxDateFormat = `${finalYear}-${finalMonth}-${day}T${hour}:${minutes}`
     let deadline = document.querySelector('#application-deadline')
     deadline.setAttribute('min', dateFormat)
+    deadline.setAttribute('max', maxDateFormat)
 });
