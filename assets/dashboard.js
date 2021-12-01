e4e1ae0e5ad260347b8a62edcd58b1f8ee4f8ba0
Mobile House <sahilshrestha77@gmail.com>
2021-12-01 10:28:18 +0545

    date-time-form

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
 
     $('.modal').modal();
+
+    var today = new Date()
+    var year = today.getFullYear()
+    var month = today.getMonth() + 1
+    var day = today.getDate()
+    var hour = today.getHours()
+    var minutes = today.getMinutes()
+    var seconds = today.getSeconds()
+
+    var dateFormat = `${year}-${month}-${day}T${hour}:${minutes}:${seconds}`
+    let deadline = document.querySelector('#application-deadline')
+    deadline.setAttribute('min', dateFormat)
 });
