af2eac1c57edd8e5e18410bdac0767cfc9cb1bc2
Mobile House <sahilshrestha77@gmail.com>
2021-12-01 14:17:04 +0545

    form

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -36,4 +36,34 @@ $(document).ready(function(){
     let deadline = document.querySelector('#application-deadline')
     deadline.setAttribute('min', dateFormat)
     deadline.setAttribute('max', maxDateFormat)
+
+    //chips
+    $('.chips').chips();
+    $('.chips-autocomplete').chips({
+        autocompleteOptions: {
+          data: {
+            'Php': null,
+            'HTML/HTML5': null,
+            'Laravel': null,
+            'React': null,
+            'JavaScript': null,
+            'Photoshop': null,
+            'UI/UX': null,
+            'Illustrator': null,
+            'Word': null,
+            'Excel': null,
+            'MongoDB': null,
+            'MySQL': null,
+            'Node.js': null,
+            'Express.js': null,
+            'Next.js': null,
+            'Git': null,
+            'Npm': null,
+            'After Effects': null,
+            'CSS/CSS3': null
+          },
+          limit: Infinity,
+          minLength: 1
+        }
+    });
 });
